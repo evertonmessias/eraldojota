2b819c7d8a2739b00da599eedb13f8808073a066
evertonmessias <everton.messias@gmail.com>
2021-12-06 11:25:06 -0300

    06-12-2021_11:25:06

---
+++ wp-content/themes/eraldojota/footer.php
@@ -0,0 +1,38 @@
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="copyright-box">
+            <p class="copyright">&copy; Copyright <strong>Eraldo Jota</strong>. All Rights Reserved</p>
+            <div class="credits">
+              <!--
+              All the links in the footer should remain intact.
+              You can delete the links only if you purchased the pro version.
+              Licensing information: https://bootstrapmade.com/license/
+              Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Eraldo Jota
+            -->
+              Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="<?php echo SITEPATH; ?>assets/vendor/purecounter/purecounter.js"></script>
+  <script src="<?php echo SITEPATH; ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo SITEPATH; ?>assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?php echo SITEPATH; ?>assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?php echo SITEPATH; ?>assets/vendor/typed.js/typed.min.js"></script>
+  <script src="<?php echo SITEPATH; ?>assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo SITEPATH; ?>assets/js/main.js"></script>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/eraldojota/header.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+	<title><?php bloginfo() ?></title>
+	<meta content="" name="description">
+	<meta content="" name="keywords">
+
+	<!-- Favicons -->
+	<link href="<?php echo SITEPATH; ?>assets/img/favicon.png" rel="icon">
+	<link href="<?php echo SITEPATH; ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<!-- Vendor CSS Files -->
+	<link href="<?php echo SITEPATH; ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo SITEPATH; ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	<link href="<?php echo SITEPATH; ?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+	<link href="<?php echo SITEPATH; ?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+	<!-- Template Main CSS File -->
+	<link href="<?php echo SITEPATH; ?>assets/css/style.css" rel="stylesheet">
+	<link href="<?php echo SITEPATH; ?>assets/css/eraldojota.css" rel="stylesheet">
+	<?php wp_head(); ?>
+
+</head>
+
+<body>
+
+	<!-- ======= Header ======= -->
+	<header id="header" class="fixed-top">
+		<div class="container d-flex align-items-center justify-content-between">
+
+			<h1 class="logo"><a href="/">Eraldo Jota</a></h1>
+			<!-- Uncomment below if you prefer to use an image logo -->
+			<!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+			<nav id="navbar" class="navbar">
+				<ul>
+					<li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+					<li><a class="nav-link scrollto" href="#about">Biografia</a></li>
+					<li><a class="nav-link scrollto" href="#services">Lançamentos</a></li>
+					<li><a class="nav-link scrollto " href="#work">Coletânea</a></li>
+					<li><a class="nav-link scrollto " href="#blog">Agenda</a></li>
+					<li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+						<ul>
+							<li><a href="#">Drop Down 1</a></li>
+							<li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+								<ul>
+									<li><a href="#">Deep Drop Down 1</a></li>
+									<li><a href="#">Deep Drop Down 2</a></li>
+									<li><a href="#">Deep Drop Down 3</a></li>
+									<li><a href="#">Deep Drop Down 4</a></li>
+									<li><a href="#">Deep Drop Down 5</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Drop Down 2</a></li>
+							<li><a href="#">Drop Down 3</a></li>
+							<li><a href="#">Drop Down 4</a></li>
+						</ul>
+					</li>
+					<li><a class="nav-link scrollto" href="#contact">Contato</a></li>
+				</ul>
+				<i class="bi bi-list mobile-nav-toggle"></i>
+			</nav><!-- .navbar -->
+
+		</div>
+	</header><!-- End Header -->
+
+	<!-- ======= Hero Section ======= -->
+	<div id="hero" class="hero route bg-image" style="background-image: url(<?php echo SITEPATH; ?>assets/img/hero-bg.jpg)">
+		<div class="overlay-itro"></div>
+		<div class="hero-content display-table">
+			<div class="table-cell">
+				<div class="container">
+					<!--<p class="display-6 color-d">Hello, world!</p>-->
+					<h1 class="hero-title mb-4">Eraldo Jota</h1>
+					<p class="hero-subtitle"><span class="typed" data-typed-items="Músico, Autor, Intérprete, Professor"></span></p>
+					<!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
+				</div>
+			</div>
+		</div>
+	</div><!-- End Hero Section -->
