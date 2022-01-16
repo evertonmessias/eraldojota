aaaff0f89587015adbc23efa6edec50626ada41e
evertonmessias <everton.messias@gmail.com>
2022-01-16 11:15:19 -0300

    16-01-2022_11:15:19

--- wp-content/themes/eraldojota/header.php
+++ wp-content/themes/eraldojota/header.php
@@ -10,7 +10,7 @@
 	<meta content="" name="keywords">
 
 	<!-- Favicons -->
-	<link href="<?php echo SITEPATH; ?>assets/img/favicon.png" rel="icon">
+	<link href="<?php echo SITEPATH; ?>assets/img/logo.png" rel="icon">
 	<link href="<?php echo SITEPATH; ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
 	<!-- Vendor CSS Files -->
