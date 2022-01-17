1e34e1cc001557be82cf302597ebc49d43cd3a65
evertonmessias <everton.messias@gmail.com>
2022-01-17 16:12:16 -0300

    17-01-2022_16:12:16

--- wp-content/themes/eraldojota/front-page.php
+++ wp-content/themes/eraldojota/front-page.php
@@ -7,7 +7,7 @@
       <div class="container">
         <!--<p class="display-6 color-d">Hello, world!</p>-->
         <h1 class="hero-title mb-4">Eraldo Jota</h1>
-        <p class="hero-subtitle"><span class="typed" data-typed-items="Músico, Autor, Intérprete, Professor"></span></p>
+        <p class="hero-subtitle"><span class="typed" data-typed-items="Músico, Compositor, Intérprete, Professor"></span></p>
         <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
       </div>
     </div>
