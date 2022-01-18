ed1a1ee473eebf6c1df635adc21757869f7af1fb
evertonmessias <everton.messias@gmail.com>
2022-01-18 17:44:37 -0300

    18-01-2022_17:44:37

--- wp-content/themes/eraldojota/header.php
+++ wp-content/themes/eraldojota/header.php
@@ -34,9 +34,7 @@
 		<div class="container d-flex align-items-center justify-content-between">
 
 			<h1 class="logo"><a href="/">Eraldo Jota</a></h1>
-			<!-- Uncomment below if you prefer to use an image logo -->
-			<!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
+			
 			<nav id="navbar" class="navbar">
 				<ul>
 					<li><a class="nav-link scrollto active" href="<?php if(!is_home())echo '/'; ?>#hero">Home</a></li>
